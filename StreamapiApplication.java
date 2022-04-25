d351df9318eac9166c0fc69eb265d51145a8ca0b
manikumar5589 <104344584+manikumar5589@users.noreply.github.com>
2022-04-25 14:47:36 +0530

    first commit
    stream api with java8+

---
+++ StreamapiApplication.java
@@ -0,0 +1,148 @@
+package com.kumar.streamapi;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamapiApplication {
+	
+	
+	static List<Employee> employees = new ArrayList<>();
+	static {
+		
+		employees.add(
+				new Employee("Mani", "Kumar", 5000.0, List.of("project 1","project 2"))
+				
+		);
+		
+		employees.add(
+				new Employee("Navaya", "Sree", 5500.0, List.of("project 2","project 3"))
+				
+		);
+		
+		employees.add(
+				new Employee("Sai", "Kalayan", 4000.0, List.of("project 1","project 4"))
+				
+		);
+		
+		employees.add(
+				new Employee("Lohitha", "Manchikalapati", 6000.0, List.of("project 1","project 2"))
+				
+		);
+	}
+
+    public static void main(String[] args) {
+
+        //SpringApplication.run(StreamapiApplication.class, args);
+    	
+    	
+    	//foreach
+    	employees.stream()
+    			.forEach(employee -> System.out.println(employee));
+    	
+    	
+    	//map
+        //collect
+        Set<Employee> increasedSal =
+                employees.stream()
+                .map(employee -> new Employee(
+                        employee.getFirstName(),
+                        employee.getLastName(),
+                        employee.getSalary()  * 1.10,
+                        employee.getProjects()
+                ))
+                .collect(Collectors.toSet());
+        System.out.println(increasedSal);
+        
+      //filter
+        //findFirst
+        List<Employee> filterEmployee =
+                employees
+                .stream()
+                .filter(employee -> employee.getSalary() > 5000.0)
+                .map(employee -> new Employee(
+                        employee.getFirstName(),
+                        employee.getLastName(),
+                        employee.getSalary()  * 1.10,
+                        employee.getProjects()
+                ))
+                .collect(Collectors.toList());
+
+        System.out.println(filterEmployee);
+
+
+       Employee firstEmployee =
+                employees
+                        .stream()
+                        .filter(employee -> employee.getSalary() > 5000.0)
+                        .map(employee -> new Employee(
+                                employee.getFirstName(),
+                                employee.getLastName(),
+                                employee.getSalary()  * 1.10,
+                                employee.getProjects()
+                        ))
+                        .findFirst()
+                        .orElse(null);
+        System.out.println(firstEmployee);
+        
+        
+        //flatMap
+        String projects =
+                employees
+                .stream()
+                .map(employee -> employee.getProjects())
+                .flatMap(strings -> strings.stream())
+                .collect(Collectors.joining(","));
+        System.out.println(projects);
+
+        //short Circuit operations
+        List<Employee> shortCircuit =
+                employees
+                .stream()
+                .skip(1)
+                .limit(1)
+                .collect(Collectors.toList());
+        System.out.println(shortCircuit);
+
+
+        //Finite Data
+        Stream.generate(Math::random)
+                .limit(5)
+                .forEach(value -> System.out.println(value));
+
+        //sorting
+        List<Employee> sortedEmployees =
+                employees
+                .stream()
+                .sorted((o1, o2) -> o1.getFirstName()
+                        .compareToIgnoreCase(o2.getFirstName()))
+                .collect(Collectors.toList());
+        System.out.println(sortedEmployees);
+
+
+        //min or max
+        employees
+                .stream()
+                .max(Comparator.comparing(Employee::getSalary))
+                .orElseThrow(NoSuchElementException::new);
+
+        //reduce
+        Double totalSal =
+                employees
+                .stream()
+                .map(employee -> employee.getSalary())
+                .reduce(0.0,Double::sum);
+        System.out.println(totalSal);
+
+    	
+    }
+
+}
